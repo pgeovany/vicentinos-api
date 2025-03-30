9f5c66face192f8819a2671d8e7127f63eac8d6b
Pablo Souza <pablo@pinaculodigital.com.br>
2025-03-30 20:53:26 -0300

    feat: implementa endpoint para criar produto

--- src/core/produto/produto.controller.ts
+++ src/core/produto/produto.controller.ts
@@ -1,9 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { ProdutoService } from './produto.service';
 import { ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { CriarProdutoDto } from './dto/criar-produto.dto';
+import { Doc } from 'src/utils/docs/doc';
+import { ProdutoComEstoqueResponseDto } from './doc/produto.response';
 
 @ApiTags('Produto')
+@UseGuards(JwtAuthGuard)
 @Controller('produto')
 export class ProdutoController {
   constructor(private readonly produtoService: ProdutoService) {}
+
+  @Doc({
+    nome: 'Criar Produto',
+    descricao: 'Cria um novo produto com estoque vazio.\n\nOBS: O nome do produto deve ser único.',
+    resposta: ProdutoComEstoqueResponseDto,
+  })
+  @Post('/')
+  async criar(@Body() data: CriarProdutoDto) {
+    return await this.produtoService.criar(data);
+  }
 }
--- src/core/produto/produto.service.ts
+++ src/core/produto/produto.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/persistencia/banco/prisma/prisma.service';
+import { CriarProdutoDto } from './dto/criar-produto.dto';
+import { AppErrorConflict } from 'src/utils/errors/app-errors';
 
 @Injectable()
-export class ProdutoService {}
+export class ProdutoService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async criar(params: CriarProdutoDto) {
+    const { nome } = params;
+
+    const produtoExistente = await this.prismaService.produto.findUnique({
+      where: { nome },
+    });
+
+    if (produtoExistente) {
+      throw new AppErrorConflict('Este produto já existe');
+    }
+
+    const produto = await this.prismaService.produto.create({
+      data: {
+        nome,
+        estoque: {
+          create: {},
+        },
+      },
+      include: {
+        estoque: true,
+      },
+    });
+
+    return {
+      ...produto,
+      estoque: produto.estoque[0],
+    };
+  }
+}
