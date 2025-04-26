bd3d14185f4dbf53613528d73d77258983b9c49a
Pablo Souza <pgeovany14@gmail.com>
2025-04-26 03:29:59 -0300

    fix: add validation for quantidade field in MovimentarEstoqueDto and RemocaoDiretaEstoqueDto

--- src/core/produto/modules/estoque/dto/movimentar-estoque.dto.ts
+++ src/core/produto/modules/estoque/dto/movimentar-estoque.dto.ts
@@ -1,5 +1,5 @@
 import { Prisma } from '@prisma/client';
-import { IsEnum, IsInt, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { IsEnum, IsInt, IsNotEmpty, IsOptional, IsPositive, IsString, Min } from 'class-validator';
 import { ENUM_TIPO_MOVIMENTACAO_ESTOQUE } from 'src/utils/enum/estoque.enum';
 
 export class MovimentarEstoqueDto {
@@ -13,6 +13,8 @@ export class MovimentarEstoqueDto {
 
   @IsNotEmpty()
   @IsInt()
+  @IsPositive()
+  @Min(1)
   quantidade: number;
 
   @IsString()
--- src/core/produto/modules/estoque/dto/remocao-direta.dto.ts
+++ src/core/produto/modules/estoque/dto/remocao-direta.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsInt, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { IsInt, IsNotEmpty, IsOptional, IsPositive, IsString, Min } from 'class-validator';
 
 export class RemocaoDiretaEstoqueDto {
   @ApiProperty({ example: 'cm8wejfta0000gn0falnw8c2s' })
@@ -10,6 +10,8 @@ export class RemocaoDiretaEstoqueDto {
   @ApiProperty({ example: 3 })
   @IsNotEmpty()
   @IsInt()
+  @IsPositive()
+  @Min(1)
   quantidade: number;
 
   @ApiProperty({ example: 'Vencimento' })
